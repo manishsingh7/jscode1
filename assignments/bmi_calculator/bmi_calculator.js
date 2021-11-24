9b4d231ca297f0e7ff2d4a9dd0fa0e7fb6f86eb1
manis <manishsinghking.ms@gmail.com>
2021-11-24 22:56:42 +0530

    jscode1

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,6 +1,18 @@
 // This function should return the BMI for a person 
 function BMICalculator(mass, height) {
     // Write your code here
+    let BMI = mass / (height**2);
+  
+  if(BMI < 18.5){
+    return "Underweight";
+  }else if (BMI < 25){
+    return "Normal";
+  }else if (BMI < 30){
+    return "Overweight";
+  }else{
+    return "Obese";
+  }
 }
 
-module.exports = BMICalculator;
+
+export default BMICalculator;
